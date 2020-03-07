1247e443661baa8c4017f001eaaa8efb20a4211d
Reza <rezaxxxx02@gmail.com>
2020-03-07 10:39:49 +0700

    Make loading in data fetch

--- Frontend/Desktop/src/side/boxTable.jsx
+++ Frontend/Desktop/src/side/boxTable.jsx
@@ -8,53 +8,76 @@ export default function BoxTable(){
 
   const [d, setD] = useState([]);
 
+  const [sta, setSta] = useState(false);
+
   const getAll = async () => {
     const temp = await get("/member/gm");    
     setD(temp);
+    setSta(true);
+  }
+
+  const checkingData = async () => {
+    if(d.length === 0){
+      await getAll();
+      setSta(true);
+    }    
+    else{
+      setSta(true);
+    }
   }
 
   useEffect(()=>{
 
-    getAll();
+    checkingData();
 
   },[]);  
+  
+  if(sta === false){
+    return(
+      <div>Loading data..</div>
+    )
+  }
 
-  return(
-    <div>
+  else{
+
+    return(
       
-      {
+      <div>      
+        {  
+          d.map((e,i)=>
+  
+            <div className="item" key={i}>
+            
+              <div className="nokop">
+                <span>{e.noKop}</span>
+              </div>
+              
+              <div className="nik">
+                <span>437</span>
+              </div>  
+              
+              <div className="idcard">
+                <span>881997001</span>
+              </div>
+              
+              <div className="nama">
+                <span>BAI SASTRA</span>
+              </div>
+              
+              <div className="sta">
+                <button><FiEdit></FiEdit></button>
+                <button><FiX></FiX></button>
+              </div>                    
+    
+            </div>
 
-        d.map((e,i)=>
+          )  
+        }     
+      </div>
 
-        <div className="item" key={i}>
-        
-          <div className="nokop">
-            <span>{e.noKop}</span>
-          </div>
-          
-          <div className="nik">
-            <span>437</span>
-          </div>  
-          
-          <div className="idcard">
-            <span>881997001</span>
-          </div>
-          
-          <div className="nama">
-            <span>BAI SASTRA</span>
-          </div>
-          
-          <div className="sta">
-            <button><FiEdit></FiEdit></button>
-            <button><FiX></FiX></button>
-          </div>                    
+    )
 
-        </div>
+  }
 
-        )
-
-      }   
-
-    </div>
-  )
+  
 }
