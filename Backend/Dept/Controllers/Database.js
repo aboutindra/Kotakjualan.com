be7c0749594a6c1947d3f7e7da1c915e656fdaf7
dev-indra <indrawanjonmuhamad@yahoo.com>
2020-03-20 02:05:32 +0700

    Get API in plant done with single endpoint

--- Backend/Dept/Controllers/Database.js
+++ Backend/Dept/Controllers/Database.js
@@ -5,11 +5,19 @@ const model = new Model();
 class DatabaseController{
     async getDept(param){
         let hasil = [];
-        if( param.id === 1 ){
-            hasil = await model.read.readAll();
-        } else if( param.id === 2 ) {
+        if( param.id === 10 ){
+            hasil = await model.read.readDept();
+        } else if( param.id === 11 ) {
             hasil = await model.read.readDataDeptWithParam( param.keyword );
-        } else {
+        } else if( param.id === 20 ) {
+            hasil = await model.read.readPlant();
+        } else if( param.id === 21 ) {
+            hasil = await model.read.readDataPlantWithParam( param.keyword );
+        } else if( param.id === 30 ) {
+            hasil = await model.read.readShop();
+        } else if( param.id === 31 ) {
+            hasil = await model.read.readDataShopWithParam( param.keyword );
+        }else {
             hasil = false;
         }
         return hasil;
--- Backend/Dept/Models/Read.js
+++ Backend/Dept/Models/Read.js
@@ -3,9 +3,11 @@ class Read{
     constructor(conDB) {
         this.db = conDB;
         this.dept = this.db.collection("Dept");
+        this.plant = this.db.collection("Plant");
+        this.shop = this.db.collection("Shop");
     }
 
-    async readAll(){
+    async readDept(){
         return await this.dept.find().sort({ noKop : -1 }).toArray();
     }
 
@@ -23,6 +25,28 @@ class Read{
         };
         console.log(await formattedObject(unWrapObject));
         let statusFind = await this.dept.find( await formattedObject(unWrapObject)).toArray();
+        if(statusFind.length !== 0 ? status = statusFind : status = {status: false, message: "Member not found"} );
+        return status;
+    }
+
+    async readPlant(){
+        return await this.plant.find().sort({ noKop : -1 }).toArray();
+    }
+
+    async readDataPlantWithParam(keyword){
+        console.log(keyword);
+        let status;
+        let generateDeleteparam = () => {
+            return Object.assign({}, ...keyword );
+        };
+        console.log(generateDeleteparam());
+        let unWrapObject = generateDeleteparam();
+        let formattedObject = async function(obj){
+            await Object.keys(obj).forEach(function(key){ if( !Number.isInteger(obj[key])){ obj[key] = new RegExp(obj[key]) } });
+            return obj;
+        };
+        console.log(await formattedObject(unWrapObject));
+        let statusFind = await this.plant.find( await formattedObject(unWrapObject)).toArray();
         if(statusFind.length !== 0 ? status = statusFind : status = {status: false, message: "Member not found"} );
         return status;
     }
